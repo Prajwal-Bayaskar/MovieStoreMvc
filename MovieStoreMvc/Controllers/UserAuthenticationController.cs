11984a87b1ef4f3aa0ed9ea2e36b7f42fdfed599
Prajwal Bayaskar <prajwalbayaskar619@gmail.com>
2024-07-23 15:14:51 +0530

    Add project files.

---
+++ MovieStoreMvc/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreMvc.Models.DTO;
+using MovieStoreMvc.Repositories.Abstract;
+
+namespace MovieStoreMvc.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        private IUserAuthenticationService authService;
+        public UserAuthenticationController(IUserAuthenticationService authService)
+        {
+            this.authService = authService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationModel register)
+        {
+            var model = new RegistrationModel
+            {
+                Email = register.Email,
+                Username = register.Username,
+                Name = register.Name,
+                Password = register.Password,
+                PasswordConfirm = register.PasswordConfirm,
+                Role = register.Role,
+            };
+            var result = await authService.RegisterAsync(model);
+            if (result != null)
+            {
+                TempData["AlertMessage"] = "User Registered Successfully";
+            }
+            return RedirectToAction("Login");
+        }
+
+        ///* To Create admin with admin rights */
+
+        //public async Task<IActionResult> Register()
+        //{
+        //    var model = new RegistrationModel
+        //    {
+        //        Email = "admin@gmail.com",
+        //        Username = "admin",
+        //        Name = "Prajwal",
+        //        Password = "Admin@123",
+        //        PasswordConfirm = "Admin@123",
+        //        Role = "Admin"
+        //    };
+        //        // we can create  register with user by changing  Role="User"
+        //    var result = await authService.RegisterAsync(model);
+        //    return Ok(result.Message);
+        //}
+
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await authService.LoginAsync(model);
+            if (result.StatusCode == 1)
+                return RedirectToAction("Index", "Home");
+            else
+            {
+                TempData["msg"] = "Could not logged in..";
+                return RedirectToAction(nameof(Login));
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+           await authService.LogoutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+
+    }
+}
